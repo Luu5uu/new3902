4f19efbfa7249ea72ea3f9f4a5a4c464882f8edd
Liuyc05 <yuanchenliu05@Gmail.com>
2026-02-20 13:41:50 -0500

    Implements of death animation in main

--- Celeste/Celeste/Input/PlayerCommand.cs
+++ Celeste/Celeste/Input/PlayerCommand.cs
@@ -8,15 +8,17 @@ namespace Celeste.Input
         public float MoveX { get; }
         public bool JumpPressed { get; }
         public bool DashPressed { get; }
+        public bool DeathPressed { get; }   // NEW
 
-        public PlayerCommand(float moveX, bool jumpPressed, bool dashPressed)
+        public PlayerCommand(float moveX, bool jumpPressed, bool dashPressed, bool deathPressed)
         {
             MoveX = moveX;
             JumpPressed = jumpPressed;
             DashPressed = dashPressed;
+            DeathPressed = deathPressed;
         }
 
-        // Arrow + WASD move; Space jump; Enter/Z/N dash.
+        // Arrow + WASD move; Space jump; Enter/Z/N dash; T death.
         public static PlayerCommand FromKeyboard(KeyboardState current, KeyboardState previous)
         {
             float moveX = 0f;
@@ -24,10 +26,14 @@ namespace Celeste.Input
             if (current.IsKeyDown(Keys.A) || current.IsKeyDown(Keys.Left))  moveX -= 1f;
 
             bool jumpPressed = current.IsKeyDown(Keys.Space) && !previous.IsKeyDown(Keys.Space);
-            bool dashPressed = (current.IsKeyDown(Keys.Enter) || current.IsKeyDown(Keys.Z) || current.IsKeyDown(Keys.N))
+
+            bool dashPressed =
+                (current.IsKeyDown(Keys.Enter) || current.IsKeyDown(Keys.Z) || current.IsKeyDown(Keys.N))
                 && !(previous.IsKeyDown(Keys.Enter) || previous.IsKeyDown(Keys.Z) || previous.IsKeyDown(Keys.N));
 
-            return new PlayerCommand(moveX, jumpPressed, dashPressed);
+            bool deathPressed = current.IsKeyDown(Keys.T) && !previous.IsKeyDown(Keys.T); // NEW
+
+            return new PlayerCommand(moveX, jumpPressed, dashPressed, deathPressed);
         }
     }
 }
