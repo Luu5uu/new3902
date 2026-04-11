da898a353136c9ec230fa6f0162429564b6d7bcc
Sihao Ren <ren.978@buckeyemail.osu.edu>
2026-04-11 15:57:57 -0400

    delete the stamina bar and resume the controller

--- Celeste/Celeste/Scenes/GameplayScene.cs
+++ Celeste/Celeste/Scenes/GameplayScene.cs
@@ -13,7 +13,6 @@ using Celeste.DevTools;
 using Celeste.Input;
 using Celeste.Items;
 using Celeste.Utils;
-using Celeste.UI;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
@@ -47,8 +46,6 @@ namespace Celeste.Scenes
         private CollisionSystem _collisionSystem;
         private HazardCollisioncs _hazardCollisionSystem;
         private Rectangle _worldBound;
-        //stamina bar
-        private ClimbStaminaBar _climbStaminaBar;
         public GameplayScene(Game1 game) : base(game)
         {
             _previousKeyboardState = Keyboard.GetState();
@@ -84,7 +81,6 @@ namespace Celeste.Scenes
 
             _pixelTexture = new Texture2D(Game.GraphicsDevice, 1, 1);
             _pixelTexture.SetData(new[] { Color.White });
-            _climbStaminaBar = new ClimbStaminaBar(_pixelTexture, 5, 36);
 
             var factory = BlockFactory.GetInstance;
             factory.LoadTextures(Game.Content);
@@ -168,31 +164,6 @@ namespace Celeste.Scenes
             DrawCollectibles(spriteBatch);
             _player.Draw(spriteBatch);
 
-            if (_player.isClimbing || _player.IsTouchingWall)
-            {
-                Vector2 barPos;
-
-                if (_player.touchingLeftWall)
-                {
-                    barPos = new Vector2(_player.position.X + 14f, _player.position.Y - 24f);
-                }
-                else if (_player.touchingRightWall)
-                {
-                    barPos = new Vector2(_player.position.X - 18f, _player.position.Y - 24f);
-                }
-                else
-                {
-                    barPos = new Vector2(_player.position.X - 18f, _player.position.Y - 24f);
-                }
-
-                _climbStaminaBar.Draw(
-                    spriteBatch,
-                    barPos,
-                    _player.ClimbStaminaPercent,
-                    _player.ClimbTiredPercent,
-                    _player.IsTired
-                );
-            }
 
             if (_debugOverlay.ShowDebug)
             {
