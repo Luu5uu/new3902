3f4b12def7090b77f141ac5ddc70a4cf75f5d9a5
Henry Fritz <133249998+hfritz34@users.noreply.github.com>
2026-04-27 11:25:25 -0400

    remove extra feather

--- Celeste/Celeste/Scenes/GameplayScene.cs
+++ Celeste/Celeste/Scenes/GameplayScene.cs
@@ -670,8 +670,6 @@ namespace Celeste.Scenes
                     new Vector2(279f, 132f),
                     CollectibleItem.ItemType.Strawberry,
                     fliesAwayOnDash: true));
-
-                _feathers.Add(CreateFlyFeather(new Vector2(520f, 220f)));
             }
 
             if (_currentRoom == 6)
